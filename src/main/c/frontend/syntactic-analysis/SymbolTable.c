f3076b78a20b9dff432b73461deaad0572e9ef1f
conradohillar <hillarconrado@gmail.com>
2025-05-29 23:43:02 -0300

    changed integer values to corresponding booleans

--- src/main/c/frontend/syntactic-analysis/SymbolTable.c
+++ src/main/c/frontend/syntactic-analysis/SymbolTable.c
@@ -1,32 +1,34 @@
 // symbol_table.c
 #include "SymbolTable.h"
 
-int insert_symbol(SymbolEntry **table, const char *name, SymbolType type) {
-    SymbolEntry *s = find_symbol(*table, name);
-    if (s != NULL) {
-        // Already defined symbol -> semantic error
-        return 0;
-    }
+boolean insert_symbol(SymbolEntry **table, const char *name, int argc,
+                      SymbolType type) {
+  SymbolEntry *s = find_symbol(*table, name);
+  if (s != NULL) {
+    // Already defined symbol -> semantic error
+    return false;
+  }
 
-    s = malloc(sizeof(SymbolEntry));
-    s->name = strdup(name);
-    s->type = type;
-    HASH_ADD_KEYPTR(hh, *table, s->name, strlen(s->name), s);
+  s = malloc(sizeof(SymbolEntry));
+  s->name = strdup(name);
+  s->type = type;
+  s->argc = argc;
+  HASH_ADD_KEYPTR(hh, *table, s->name, strlen(s->name), s);
 
-    return 1;
+  return true;
 }
 
 SymbolEntry *find_symbol(SymbolEntry *table, const char *name) {
-    SymbolEntry *s;
-    HASH_FIND_STR(table, name, s);
-    return s;
+  SymbolEntry *s;
+  HASH_FIND_STR(table, name, s);
+  return s;
 }
 
 void free_symbol_table(SymbolEntry **table) {
-    SymbolEntry *s, *tmp;
-    HASH_ITER(hh, *table, s, tmp) {
-        HASH_DEL(*table, s);
-        free(s->name);
-        free(s);
-    }
+  SymbolEntry *s, *tmp;
+  HASH_ITER(hh, *table, s, tmp) {
+    HASH_DEL(*table, s);
+    free(s->name);
+    free(s);
+  }
 }
